9e198ab992d242095522590b90884f4f1e4a0c88
Skyler Rain Ross <orangesnowfox@gmail.com>
2025-06-19 03:11:07 -0700

    chore: flakeify
    ... need it for dev work

---
+++ flake.nix
@@ -0,0 +1,41 @@
+# This file is pretty general, and you can adapt it in your project replacing
+# only `name` and `description` below.
+
+{
+  description = "rasen";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, utils, rust-overlay, ... }:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in
+        rec {
+          # `nix develop`
+          devShell = pkgs.mkShell
+            {
+              buildInputs = with pkgs;
+                # Tools you need for development go here.
+                [
+                  (rust-bin.selectLatestNightlyWith
+                    (toolchain: toolchain.default.override {
+                      extensions = [ "rust-src" ];
+                    })
+                  )
+                  nixpkgs-fmt
+                  cargo-outdated
+                  cargo-edit
+                ];
+            };
+        }
+      );
+}
